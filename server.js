528e244f6400f5f5c8e478972eaf1e80a3c9af42
Thomas <30468516+TomEsDE@users.noreply.github.com>
2022-09-04 01:14:28 +0200

    test-2

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const { NODE_ENV, TEST } = process.env;
+const { NODE_ENV, TEST, TEST2 } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hellooooo World! Test: ' + TEST);
+  res.send('Hellooooo World! TEST2: ' + TEST2);
 });
 
 app.listen(port, () => {
