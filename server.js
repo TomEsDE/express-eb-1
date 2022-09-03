6a5ea31b61d4f55fb8bc42bd5e9597ec5323e218
Thomas <30468516+TomEsDE@users.noreply.github.com>
2022-09-04 00:14:58 +0200

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const { NODE_ENV } = process.env;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hellooooo World!');
+});
+
+app.listen(port, () => {
+  console.log(
+    `👂 App listening at http://localhost:${port} - ENV: ${NODE_ENV}`
+  );
+});
